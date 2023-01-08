a642ad44d84577860ddaa79d0b3d4bb607d7d936
Olesia Romanienkova <110686374+Romanienkova@users.noreply.github.com>
2023-01-08 21:57:46 +0200

    1, 2

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,29 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+const divWithGalleryId = document.querySelector('.gallery');
+divWithGalleryId.innerHTML = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `
+	<a class="gallery__item" href="${original}">
+	<img
+	class="gallery__image"
+	src="${preview}"
+	alt="${description}"
+	/>
+	</a>
+	`
+  )
+  .join('');
+
+new SimpleLightbox('.gallery .gallery__item', {
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
